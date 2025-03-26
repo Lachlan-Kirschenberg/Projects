f1330ba89fb0086b42ed0a9548da1de410319ca4
Lachlan Kirschenberg <kirschenberglachlan@gmail.com>
2025-03-26 12:36:53 +1100

    Update user_churn_rate.sql

--- User_Churn_Rate/user_churn_rate.sql
+++ User_Churn_Rate/user_churn_rate.sql
@@ -1,3 +1,4 @@
+-- Define a set of months with their first and last day
 WITH months AS (
     SELECT DATE('2017-01-01') AS first_day, DATE('2017-01-31') AS last_day
     UNION
@@ -6,33 +7,39 @@ WITH months AS (
     SELECT DATE('2017-03-01'), DATE('2017-03-31')
 ), 
 
+-- Cross join the subscriptions table with the months table to create all possible combinations
 cross_join AS (
     SELECT * 
     FROM subscriptions 
     CROSS JOIN months
 ), 
 
+-- Determine the status of each subscription for each month 
 status AS (
     SELECT 
         id,
-        first_day AS month,
+        first_day AS month, -- Assigning the month's first day as the reference point
         CASE 
-            WHEN segment = 87 
+            WHEN segment = 87 -- Check if the subscription is active in segment 87
             AND subscription_start < first_day 
             AND (subscription_end IS NULL OR subscription_end > first_day) 
             THEN 1 ELSE 0 
         END AS is_active_87,
         CASE 
-            WHEN segment = 30 
+            WHEN segment = 30  -- Check if the subscription is active in segment 30
             AND subscription_start < first_day 
             AND (subscription_end IS NULL OR subscription_end > first_day) 
             THEN 1 ELSE 0 
         END AS is_active_30,
+        
+        -- Check if the subscription was canceled in segment 87 during the month
         CASE 
             WHEN segment = 87 
             AND subscription_end BETWEEN first_day AND last_day 
             THEN 1 ELSE 0 
         END AS is_canceled_87,
+
+         -- Check if the subscription was canceled in segment 30 during the month
         CASE 
             WHEN segment = 30 
             AND subscription_end BETWEEN first_day AND last_day 
@@ -41,6 +48,7 @@ status AS (
     FROM cross_join
 ), 
 
+-- Aggregate the active and canceled subscriptions per month
 status_aggregate AS (
     SELECT 
         month, 
@@ -52,6 +60,7 @@ status_aggregate AS (
     GROUP BY month
 ) 
 
+-- Calculate the churn rate for each segment
 SELECT 
     month, 
     100.0 * sum_canceled_30 / CASE WHEN sum_active_30 = 0 THEN NULL ELSE sum_active_30 END AS churn_rate_30,
